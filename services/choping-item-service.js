dd8c49e7b785c87fb87fa6a62630ee3d8d709587
AlexandrL <alexandrl@cet.ac.il>
2021-01-04 09:59:22 +0200

    add chopping item service

---
+++ services/choping-item-service.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const ChoppingItemSchema = require('../models/chopingItem');
+
+const ChoppingItem = mongoose.model('User', ChoppingItemSchema);
+
+
+const addChoppingItem = async (item) => {
+    try{
+        //ADD TOTAL PRICE
+        // item.totalPrice = xxxxxx
+        const newItem = new ChoppingItem(item);
+        await newItem.save()
+    }catch(err){  console.log(err)  }
+}
+
+const deleteChoppingItem = async (id) => {
+    return await ChoppingItem.deleteOne({_id: id});
+}
+
+const updateChopingCart = async (id, params) => {
+    return await ChoppingItem.updateOne(
+        {_id: id},
+        {$set: params}
+    );
+}
+
+
+const isItemExist = async (item) => {
+    const itemExist = await ChoppingItem.findOne(item);
+    if(itemExist){
+        return itemExist
+    }else{
+        return false
+    }
+}
+
+
+module.exports = {
+    addChoppingItem,
+    deleteChoppingItem,
+    updateChopingCart,
+    isItemExist
+}
+
